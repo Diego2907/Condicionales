0b0c5d40ca781f16b241ec4d979b5780e4dc36eb
Diego2907 <144285327+Diego2907@users.noreply.github.com>
2023-10-09 18:47:38 -0600

    Add files via upload

---
+++ Condicionales/main.js
@@ -0,0 +1,25 @@
+const nombre="Diego";
+if (nombre=="Diego"){
+    console.log("Hola Diego");
+}
+
+
+
+// const edad=10;
+// switch(edad) {
+//     case 0:
+//         console.log("Recien nacido")
+//         break;
+//     case 10:
+//         console.log("menor de edad")
+//         break;
+//     case 20:
+//         console.log("mayor de edad")
+//         break;
+//     case 70:
+//         console.log("tercera edad")
+//         break;
+//     default:
+//         console.log("¿Hola?");
+//         break;        
+// }
